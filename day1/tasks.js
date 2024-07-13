70a25f1ef3c6fbe15e4819a51d7d32a0556fabb1
Sounak Saha <sounaksaha999@gamil.com>
2024-07-14 00:54:33 +0530

    day1 tasks

---
+++ day1/tasks.js
@@ -0,0 +1,37 @@
+// Activity 1: Variable Declaration
+var num = 12
+console.log(num)
+
+let input = "Sounak"
+console.log(input)
+
+// Activity 2: Const Declaration
+const flag = true
+console.log(flag)
+
+// Activity 3: Data Types
+let arr = [2, 3, 5, 1, 0]
+let obj = {
+    id: 1,
+    dept: "CS"
+}
+
+console.log(typeof num)
+console.log(typeof input)
+console.log(typeof flag)
+console.log(typeof arr)
+console.log(typeof obj)
+
+// Activity 4: Reassigning Variables
+let reAssign = 23
+console.log(reAssign)
+
+reAssign = "Ankan"
+console.log(reAssign)
+
+// Activity 5: Understanding const
+const reConst = 55
+console.log(reConst)
+
+// reConst = "1"
+// console.log(reConst)
