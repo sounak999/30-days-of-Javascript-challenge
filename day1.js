516cb4b3677ba3bdaed3621a6ac0b9e4a7f9cc69
Sounak Saha <sounaksaha999@gamil.com>
2024-07-13 15:57:34 +0530

    day1: activity 3

--- day1.js
+++ day1.js
@@ -8,3 +8,16 @@ console.log(input)
 // Activity 2: Const Declaration
 const flag = true
 console.log(flag)
+
+// Activity 3: Data Types
+let arr = [2, 3, 5, 1, 0]
+let obj = {
+    id: 1,
+    dept: "CS"
+}
+
+console.log(typeof num)
+console.log(typeof input)
+console.log(typeof flag)
+console.log(typeof arr)
+console.log(typeof obj)
