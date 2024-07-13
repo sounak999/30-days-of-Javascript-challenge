30bb39a33fa3a32f86841e84639e970bde9d8a4b
Sounak Saha <sounaksaha999@gamil.com>
2024-07-13 15:54:59 +0530

    day 1 activities

---
+++ day1.js
@@ -0,0 +1,10 @@
+// Activity 1: Variable Declaration
+var num = 12
+console.log(num)
+
+let input = "Sounak"
+console.log(input)
+
+// Activity 2: Const Declaration
+const flag = true
+console.log(flag)
