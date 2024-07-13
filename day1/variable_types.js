3695edaee438c0e47c24d36025c688c3e0bc2b31
Sounak Saha <sounaksaha999@gamil.com>
2024-07-14 01:15:05 +0530

    Varible Types console log feature request

---
+++ day1/variable_types.js
@@ -0,0 +1,38 @@
+// nnssbbu
+
+// 1. number
+let num = 23, num2 = 5.7
+console.log(num + ", " + num2)
+console.log(typeof num)
+
+// 2. null
+const constant = null
+console.log(constant)
+console.log(typeof constant)
+
+// 3. string
+let job = "Engineer"
+console.log(job)
+console.log(typeof job)
+
+// 4. symbol
+const sym = Symbol()
+const sym2 = Symbol('234fsdl')
+console.log(sym)
+console.log(sym2)
+console.log(typeof sym)
+
+// 5. bigint
+let x = BigInt("123456789012345678901234567890")
+console.log(x)
+console.log(typeof x)
+
+// 6. boolean
+let flag = false
+console.log(flag)
+console.log(typeof flag)
+
+// 7. undefined
+let un = undefined
+console.log(un)
+console.log(typeof un)
