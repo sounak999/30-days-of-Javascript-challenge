8b2d8479d3f93a4c8d1a954b16d9c0dd7826ecea
Sounak Saha <sounaksaha999@gamil.com>
2024-07-22 20:30:30 +0530

    activity 2 completed

--- day9 - dom/script.js
+++ day9 - dom/script.js
@@ -8,7 +8,17 @@ htmlId.textContent = `Task 1: text content changed by capturing HTML element's I
 const htmlCls = document.getElementsByClassName('highlight')[0]
 htmlCls.style.backgroundColor = 'yellow'
 
+
 // Activity 2: Creating and Appending Elements
 
 // Task 3: Create a new div element with some text content and append it to the body.
+const newDiv = document.createElement("div");
+newDiv.textContent = 'New Div Element added as per Task 3';
+document.body.append(newDiv);
+
 // Task 4: Create a new li element and add it to an existing ul list.
+const ul = document.getElementById('item-list')
+const newList = document.createElement('li')
+let textNode = document.createTextNode('New item 3 added')
+newList.appendChild(textNode)
+ul.appendChild(newList)
